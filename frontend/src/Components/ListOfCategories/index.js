cdec6f1171888b12c8efd9b1a02d179da758a144
Irungaray <LionelIrungaray@GMail.com>
2021-03-01 11:37:53 -0300

    Added IntersectionObserver for LazyLoading

--- frontend/src/Components/ListOfCategories/index.js
+++ frontend/src/Components/ListOfCategories/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Category } from '../Category';
 
-import { categories } from '../../../../api/db.json';
-
 import { List, Item } from './styles';
 
+function useCategoriesData() {
+	const [ categories, setCategories ] = useState([]);
+	const [ loading, setLoading ] = useState(false);
+
+	useEffect(function () {
+		setLoading(true)
+		window.fetch('https://petgram-server-24iykciv5.now.sh/categories')
+			.then(res => res.json())
+			.then(response => {
+				setCategories(response)
+				setLoading(false)
+			})
+	}, [])
+
+	return { categories, loading };
+}
+
 export const ListOfCategories = () => {
+
+	const { categories, loading } = useCategoriesData()
+
+	if (loading) {
+		return 'Loading...'
+	}
+
 	return (
 		<List>
 			{
--- frontend/src/Components/PhotoCard/index.js
+++ frontend/src/Components/PhotoCard/index.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { MdFavoriteBorder } from 'react-icons/md';
 
-import { ImgWrapper, Img, Button } from './styles';
+import { Article, ImgWrapper, Img, Button } from './styles';
 
 const DEFAULT_IMAGE = "https://images.unsplash.com/photo-1548767797-d8c844163c4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
 
-      <Button>
-        <MdFavoriteBorder size='32px'/> {likes} likes
-      </Button>
-    </article>
+  const ref = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(function () {
+    const observer = new window.IntersectionObserver(
+      function (entries) {
+        const { isIntersecting } = entries[0]
+
+        if (isIntersecting) {
+          console.log('Intersecting')
+          setShow(true)
+          observer.disconnect()
+        }
+      }
+    )
+
+    observer.observe(ref.current)
+  }, [ref])
+
+  return (
+    <Article ref={ref}>
+      {
+        show &&
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes
+          </Button>
+        </>
+      }
+    </Article>
   )
 }
