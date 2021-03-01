1bb0bd1cc62077e935e4bfc626dbd7ea44aff408
Irungaray <LionelIrungaray@GMail.com>
2021-03-01 08:59:41 -0300

    Added PhotoCard styled component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ListOfCategories } from './Components/ListOfCategories';
+import { ListOfPhotoCards } from './Components/ListOfPhotoCards';
 
 import { GlobalStyles } from './GlobalStyles';
 
@@ -9,9 +10,11 @@ const App = () => {
 		<>
 			<GlobalStyles />
 			<ListOfCategories />
+			<ListOfPhotoCards />
 		</>
 	);
 };
 
 
 export default App;
+
