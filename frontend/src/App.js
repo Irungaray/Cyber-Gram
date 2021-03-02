2eeb99f6ecf0250a327c9ce4ebf2620a1fe862d0
Irungaray <LionelIrungaray@GMail.com>
2021-03-02 12:00:53 -0300

    Using Render Props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,29 @@ import { Logo } from './Components/Logo'
 import { ListOfCategories } from './Components/ListOfCategories';
 
 import { ListOfPhotoCards } from './containers/ListOfPhotoCards';
+import { PhotoCardWithQuery } from './containers/PhotoCardWithQuery';
+
 
 import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
+	const urlParams = new window.URLSearchParams(window.location.search);
+	const detailId = urlParams.get('detail');
+
+	// console.log(detailId)
+
 	return (
 		<>
 			<GlobalStyles />
 			<Logo />
-			<ListOfCategories />
-			<ListOfPhotoCards categoryId={1} />
+			{
+				detailId
+					? <PhotoCardWithQuery id={detailId} />
+					: <>
+						<ListOfCategories />
+						<ListOfPhotoCards categoryId={1} />
+					</>
+			}
 		</>
 	);
 };
